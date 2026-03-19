3faf514d74f2c93a9a0bc724f764821e7921eb0b
Peter Edwards <p.l.edwards@leeds.ac.uk>
2026-03-19 12:28:33 +0000

    Added awesome markers but not tested yet. Removed a load of stuff which isnt used

--- _scripts/minify.js
+++ _scripts/minify.js
@@ -6,21 +6,13 @@ const path = require('path');
 const UglifyJS = require("uglify-js");
 const sass = require('sass');
 
-const compiled_js = path.resolve( __dirname, '../assets/js/maps.js' );
-const minified_js = path.resolve( __dirname, '../assets/js/maps.min.js' );
-const compiled_css = path.resolve( __dirname, '../assets/css/maps.css' );
-const minified_css = path.resolve( __dirname, '../assets/css/maps.min.css' );
+const minified_js = path.resolve( __dirname, '../assets/js/script.min.js' );
+const minified_css = path.resolve( __dirname, '../assets/css/style.min.css' );
 const jsdir = '../_includes/scripts/';
 
-var result = sass.compile( path.resolve( __dirname, '../_scss/maps.scss' ) );
-fs.writeFileSync( compiled_css, result.css );
 result = sass.compile( path.resolve( __dirname, '../_scss/maps.scss' ), {style: "compressed"} );
 fs.writeFileSync( minified_css, result.css );
 
-fs.writeFileSync( compiled_js, '');
-for ( const file of [ 'leaflet.fullscreen.js', 'utilities.js', 'maps.js', 'features.js' ] ) {
-    fs.appendFileSync( compiled_js, fs.readFileSync( path.resolve( __dirname, jsdir, file ), "utf8" ) );
-}
 fs.writeFileSync( minified_js, UglifyJS.minify({
     "leaflet.fullscreen.js": fs.readFileSync( path.resolve( __dirname, jsdir, 'leaflet.fullscreen.js' ), "utf8" ),
     "utilities.js": fs.readFileSync( path.resolve( __dirname, jsdir, 'utilities.js' ), "utf8" ),
---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+---
+---
+{% include scripts/leaflet.fullscreen.js %}
+{% include scripts/leaflet.awesome-markers.min.js %}
+{% include scripts/utilities.js %}
+{% include scripts/maps.js %}
+{% include scripts/features.js %}
